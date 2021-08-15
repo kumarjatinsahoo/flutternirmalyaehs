f7419257e77c646874647637d8b156bd98913660
Lisaranee Nath <lisaranee94@gmail.com>
2021-08-15 14:17:46 +0530

    change by lisa

--- lib/screens/TestAppointmentPage.dart
+++ lib/screens/TestAppointmentPage.dart
@@ -119,7 +119,8 @@ class _TestAppointmentPageState extends State<TestAppointmentPage>
     //if (picked != null && picked != selectedDate)
     setState(() {
       isDataNotAvail=false;
-      final df = new DateFormat('yyyy/MM/dd');
+      final df = new DateFormat('dd/MM/yyyy');
+      //final df = new DateFormat('yyyy/MM/dd');
       today = df.format(picked);
       callAPI(today);
     });
