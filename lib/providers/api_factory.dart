1504eaffeb0b196ed92af9661002d3cb0e9555d7
sanjayajena37 <sanjayajena00@gmail.com>
2021-09-20 13:59:52 +0530

    by sanjaya

--- lib/providers/api_factory.dart
+++ lib/providers/api_factory.dart
@@ -1,7 +1,5 @@
 class ApiFactory {
   static String REG_DEVICE = "https://cca.medtel.in/Ziniai/manageDeviceId";
-  //static String MAIN_URL = "http://api.ehealthsystem.com/nirmalyaRest/api/";
-  static String MAIN_URL = "http://192.168.29.110:8062/nirmalyaRest/api/";
 
   static String MAIN_URL = "http://api.ehealthsystem.com/nirmalyaRest/api/";
   //static String MAIN_URL = "http://192.168.29.107:8062/nirmalyaRest/api/";
