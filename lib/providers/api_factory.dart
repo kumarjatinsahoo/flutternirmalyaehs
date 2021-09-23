37fcee41c9c2ac466715626706459b66342f527f
Ipsita Sahoo <sahoo.ipsaita@gmail.com>
2021-09-23 11:28:55 +0530

    change by ipsita

--- lib/providers/api_factory.dart
+++ lib/providers/api_factory.dart
@@ -1,7 +1,7 @@
 class ApiFactory {
   static String REG_DEVICE = "https://cca.medtel.in/Ziniai/manageDeviceId";
  // static String MAIN_URL = "http://api.ehealthsystem.com/nirmalyaRest/api/";
-  static String MAIN_URL = "http://192.168.29.113:8062/nirmalyaRest/api/";
+  static String MAIN_URL = "http://192.168.29.27:8062/nirmalyaRest/api/";
 
   static String VITALS_REPORT = MAIN_URL + 'medtel-screening-test-report';
   static String COUNTRY_API = MAIN_URL + 'get-country-list';
@@ -78,6 +78,7 @@ static String GOOGLE_LOC(
   static String HEALTH_APPOINTMENT_CHKUP_LIST =MAIN_URL + 'view-user-checkup-appointment-list?userid=';
   static String USER_APPOINTMENT_LIST =MAIN_URL + 'get-user-appointment-list?userid=';
   static String doctor_APPOINTMENT_LIST =MAIN_URL + 'view-doctor-appointment-list?userid=';
+  static String doctor_MEDICINE_LIST =MAIN_URL + 'view-user-medicine-details-by-appno?appno=';
   static String user_APPOINTMENT_status =MAIN_URL + 'change-user-appointment-status?appid=';
   static String TEST_REPORT_USER = MAIN_URL + 'view-medteltest-list-throughId';
   static String USER_APPOINTMENTS = MAIN_URL + 'get-user-appointment-list?userid=';
