137ec0d7688c06bb7f34d7b65aeb91fbdffbcf61
sanjayajena37 <sanjayajena00@gmail.com>
2021-10-18 15:53:35 +0530

    BySanjaya

--- android/app/src/main/kotlin/com/ehealthsystem/user/MainActivity.kt
+++ android/app/src/main/kotlin/com/ehealthsystem/user/MainActivity.kt
@@ -19,16 +19,19 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     private val CHANNEL = "com.eHealthSystem/goAnotherApp"
 
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
             if (call.method == "getBatteryLevel") {
 
-            }else if (call.method == "callUrl") {
+            } else if (call.method == "callUrl") {
                 //val intent = Intent(this, WebViewActivity::class.java)
                 //intent.putExtra("key", value)
                 //startActivity(intent)
@@ -47,7 +50,7 @@ class MainActivity: FlutterActivity() {
                         val string: String = call.arguments as String
                         val data: List<String> = string.split(",")
                         val LaunchIntent = packageManager
-                                .getLaunchIntentForPackage("ilabmini.in.ilab")
+                            .getLaunchIntentForPackage("ilabmini.in.ilab")
                         LaunchIntent!!.action = Intent.ACTION_SEND
                         LaunchIntent.putExtra(Intent.EXTRA_TEXT, "1")
                         LaunchIntent.putExtra("apid", "8036d1868bd71fb6b500cb18eeec3936")
@@ -59,14 +62,18 @@ class MainActivity: FlutterActivity() {
                         LaunchIntent.putExtra("weight", data.get(5))
                         LaunchIntent.putExtra("age", data.get(6))
                         LaunchIntent.type = "text/plain"
-                        //startActivity(LaunchIntent)
-
-                        Toast.makeText(this@MainActiivty, "Height: "+data.get(4)+" Weight: "+data.get(5), Toast.LENGTH_SHORT).show()
+                        val t = Toast.makeText(
+                            this@MainActivity,
+                            "Height: " + data.get(4) + " Weight: " + data.get(5),
+                            Toast.LENGTH_SHORT
+                        )
+                        t.show()
+                        startActivity(LaunchIntent)
 
                     } else {
-                        val string: String = call.arguments as String
+                        /*val string: String = call.arguments as String
                         val data: List<String> = string.split(",")
-                        Toast.makeText(this@MainActiivty, "Height: "+data.get(4)+" Weight: "+data.get(5), Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActiivty, "Height: "+data.get(4)+" Weight: "+data.get(5), Toast.LENGTH_SHORT).show()*/
                         redirectToPlayStore()
 
                         // Do whatever we want to do if application not installed
@@ -75,35 +82,31 @@ class MainActivity: FlutterActivity() {
                         // Log.i("Application is not currently installed.");
                     }
                 } else {
-                    val string: String = call.arguments as String
-                    val data: List<String> = string.split(",")
-                    Toast.makeText(this@MainActiivty, "Height: "+data.get(4)+" Weight: "+data.get(5), Toast.LENGTH_SHORT).show()
+                    var dialog = Dialog(this@MainActivity)
+                    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE) //before
+                    dialog.setContentView(R.layout.noapkdialog)
+                    val downloadbtn: Button = dialog.findViewById(R.id.downloadbtn) as Button
+                    /*downloadbtn.setOnClickListener(object : View.OnClickListener() {
+                        fun onClick(view: View?) {
+                            redirectToPlayStore()
+                            //   Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.janacare.aina.production"));
+                            // startActivity(intent);
+                        }
+                    })*/
+                    downloadbtn.setOnClickListener(object : View.OnClickListener {
+                        override fun onClick(view: View?) {
+                            // Do some work here
+                            redirectToPlayStore()
+                        }
 
-//                    var dialog = Dialog(this@MainActivity)
-//                    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE) //before
-//                    dialog.setContentView(R.layout.noapkdialog)
-//                    val downloadbtn: Button = dialog.findViewById(R.id.downloadbtn) as Button
-//                    /*downloadbtn.setOnClickListener(object : View.OnClickListener() {
-//                        fun onClick(view: View?) {
-//                            redirectToPlayStore()
-//                            //   Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.janacare.aina.production"));
-//                            // startActivity(intent);
-//                        }
-//                    })*/
-//                    downloadbtn.setOnClickListener(object : View.OnClickListener {
-//                        override fun onClick(view: View?) {
-//                            // Do some work here
-//                            redirectToPlayStore()
-//                        }
-//
-//                    })
-//                    val cancelbtn: Button = dialog.findViewById(R.id.cancelbtn) as Button
-//                    cancelbtn.setOnClickListener(object : View.OnClickListener {
-//                        override fun onClick(view: View?) {
-//                            dialog.dismiss()
-//                        }
-//                    })
-//                    dialog.show()
+                    })
+                    val cancelbtn: Button = dialog.findViewById(R.id.cancelbtn) as Button
+                    cancelbtn.setOnClickListener(object : View.OnClickListener {
+                        override fun onClick(view: View?) {
+                            dialog.dismiss()
+                        }
+                    })
+                    dialog.show()
                 }
 
             } else {
@@ -114,7 +117,8 @@ class MainActivity: FlutterActivity() {
 
 
     fun redirectToPlayStore() {
-        val url = "https://drive.google.com/file/d/1iB4IUeT4vOV9lCRiEwilbHLYA-DBlgJ2/view?usp=sharing"
+        val url =
+            "https://drive.google.com/file/d/1iB4IUeT4vOV9lCRiEwilbHLYA-DBlgJ2/view?usp=sharing"
         // String url="https://drive.google.com/file/d/1cDKAENDCdOOzyk9jwOtHjRs8gE3PVxW8/view?usp=sharing";
         val i = Intent(Intent.ACTION_VIEW)
         i.data = Uri.parse(url)
@@ -152,7 +156,8 @@ class MainActivity: FlutterActivity() {
             val mTelephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) !=
-                        PackageManager.PERMISSION_GRANTED) {
+                    PackageManager.PERMISSION_GRANTED
+                ) {
                     return ""
                 }
             }
@@ -164,12 +169,17 @@ class MainActivity: FlutterActivity() {
                     mTelephony.deviceId
                 }
             } else {
-                Settings.Secure.getString(context.contentResolver,
-                        Settings.Secure.ANDROID_ID)
+                Settings.Secure.getString(
+                    context.contentResolver,
+                    Settings.Secure.ANDROID_ID
+                )
             }
         }
-        if (deviceId == "" || deviceId==null) {
-            val android_id = Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
+        if (deviceId == "" || deviceId == null) {
+            val android_id = Settings.Secure.getString(
+                applicationContext.contentResolver,
+                Settings.Secure.ANDROID_ID
+            )
             deviceId = android_id
         }
         return deviceId
