3c0d78c84ff236bf272d21e48ee3d35c28ec1fee
sanjayajena37 <sanjayajena37@gmail.com>
2021-10-21 19:40:07 +0530

    change by lisa

--- lib/providers/api_factory.dart
+++ lib/providers/api_factory.dart
@@ -5,7 +5,7 @@ class ApiFactory {
   static String MAIN_URL1 = "https://1331.co.in/api";
  // static String MAIN_URL = "http://192.168.43.248:8062/nirmalyaRest/api/";
  static String MAIN_URL = "http://192.168.43.248:8062/nirmalyaRest/api/";
-  static String MAIN_URL = "http://192.168.29.16:8062/nirmalyaRest/api/";
+  //static String MAIN_URL = "http://192.168.29.16:8062/nirmalyaRest/api/";
  //static String MAIN_URL = "http://192.168.43.248:8062/nirmalyaRest/api/";
   static String VITALS_REPORT = MAIN_URL + 'medtel-screening-test-report';
   static String COUNTRY_API = MAIN_URL + 'get-country-list';
