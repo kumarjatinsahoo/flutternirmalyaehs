d80bacc921c8744cd1d5499ba04367776550072f
sanjayajena37 <sanjayajena37@gmail.com>
2021-11-01 19:26:05 +0530

    change by lisa

--- lib/screens/Doctor/registartion/DoctorSignUpForm3.dart
+++ lib/screens/Doctor/registartion/DoctorSignUpForm3.dart
@@ -266,9 +266,10 @@ class DoctorSignUpForm3State extends State<DoctorSignUpForm3> {
                             ],
                           )
                         ],
-                    /*  SizedBox(
+                      ),
+                      SizedBox(
                         height: 5,
-                      ),*/
+                      ),
                       Padding(
                         padding: const EdgeInsets.only(left: 8.0),
                         child: Row(
