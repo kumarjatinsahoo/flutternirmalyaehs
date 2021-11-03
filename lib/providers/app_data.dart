e4694851076e6ebdebf4035a111388764614be21
Suprava Swain <rohandarkbull@gmail.com>
2021-11-03 12:10:02 +0530

    commit by ipsita

--- lib/providers/app_data.dart
+++ lib/providers/app_data.dart
@@ -512,8 +512,8 @@ class AppData {
     return base64.encode(utf8.encode(data));
   }
 
-  static String address = "Address: 1073, Bhosale Mystiqa,Gokhale Road,\n"
-      "Model Colony,Pune - 411016, (MH) INDIA";
+  static String address = "Address: 1073, Bhosale Mystiqa,Gokhale,\n"
+      "Road Model Colony,Pune - 411016, (MH) INDIA";
   static String aboutus =
       "Together, we’re on a path to wellness for body, mind, and spirit. Now anyone can take charge of their health and transform their life using our easy-to-use, yet powerful, healthcare technologies & tools.The eHealthSystem supports WHO eHealth Strategy (WHA 58:28) eHealth stressing that eHealth is the cost-effective and secure use of information and communications technologies in support of health and health-related services. \n"
       "eHealthSystem works as a LiveWire of Connectivity connecting different healthcare stakeholders that include clinics, pharmacies, blood banks, hospitals, ambulances, organ donors, laboratories and patients on a single platform."
