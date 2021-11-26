edb3637f372e1a6c81adbc1da56d9d354a5714dc
sanjayajena37 <sanjayajena37@gmail.com>
2021-11-26 19:30:50 +0530

    change by lisa

--- lib/providers/api_factory.dart
+++ lib/providers/api_factory.dart
@@ -26,8 +26,8 @@ class ApiFactory {
   static String BIOMEDICAL_IMPLANTS = MAIN_URL + 'view-user-biomedical-implant-list-api?userid=';
   static String lab_list_by_searchvalue = MAIN_URL + 'get-lab-list-by-searchvalue?search=';
 
-  static String BIOMEDICAL_IMPLANTS =
-      MAIN_URL + 'view-user-biomedical-implant-list-api?userid=';
+  /*static String BIOMEDICAL_IMPLANTS =
+      MAIN_URL + 'view-user-biomedical-implant-list-api?userid=';*/
   static String UPLOAD_DOCUMENT = MAIN_URL + 'get-medical-upload-details-api?userid=';
   static String DIESEINFO = MAIN_URL + 'get-disease-info-list';
   static String IMMUNIZATION_LIST =
