eaee6a40fccae55e3b9ec7379eb4a3f430117f5c
LENOVO-PC <nayaksuvam26@gmail.com>
2022-01-17 19:35:07 +0530

    Date-17-jan-22 By Suvam

--- lib/providers/api_factory.dart
+++ lib/providers/api_factory.dart
@@ -10,7 +10,7 @@ class ApiFactory {
    // static String MAIN_URL = "http://192.168.137.1:8062/nirmalyaRest/api/";
    //static String MAIN_URL = "http://192.168.29.137.1:8062/nirmalyaRest/api/";
    //static String MAIN_URL = "http://192.168.137.1:8062/nirmalyaRest/api/";
-   //static String MAIN_URL = "http://192.168.43.248:8062/nirmalyaRest/api/";
+   static String MAIN_URL = "http://192.168.43.56:8062/nirmalyaRest/api/";
   static String VITALS_REPORT = MAIN_URL + 'medtel-screening-test-report';
   static String COUNTRY_API = MAIN_URL + 'get-country-list';
   static String VIEW_PATIENT_HEALTH_API = REPORT_URL1 + 'user/mobile-view-patient-health-chart?id=';
