72cdb51502eb50b0a50f90f4b2a85a049147b8cd
sanjayajena37 <sanjayajena37@gmail.com>
2022-01-18 16:27:19 +0530

    change by lisa

--- lib/providers/Const.dart
+++ lib/providers/Const.dart
@@ -67,7 +67,6 @@ class Const {
   static const String NEXT_SERVER_LOAD = "next_server_load";
   static const String ALREADY_REG_STATUS = "registered";
   static const String APP_ID = "8036d1868bd71fb6b500cb18eeec3936";
-
   static String DOC_APNT = "DOC_APNT";
   static String HEALTH_SCREENING_APNT = "HEALTH_SCREENING_APNT";
   static String CONFIRMED = "CONFIRMED";
